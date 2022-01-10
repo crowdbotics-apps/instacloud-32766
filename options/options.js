4d3b3526fa4909e3176c365dcc455d1d7445c7e7
Crowdbotics <team@crowdbotics.com>
2022-01-10 07:28:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "instacloud_32766",
+  url: "https://instacloud_32766.botics.co",
+  api: "https://instacloud_32766.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
